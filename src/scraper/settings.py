785d3b569fb99f539b10ec6e906ceb4e6c95e502
bgozlinski <bartlomiej.gozlinski@gmail.com>
2026-03-24 14:35:55 +0100

    feat: implemetn Scrapy pipeline with Django ORM integration

--- src/scraper/settings.py
+++ src/scraper/settings.py
@@ -3,3 +3,7 @@ SPIDER_MODULES = ['scraper.spiders']
 NEWSPIDER_MODULE = 'scraper.spiders'
 ROBOTSTXT_OBEY = False
 DOWNLOAD_DELAY = 0.5
+
+ITEM_PIPELINES = {
+    'scraper.pipelines.ArticlePipeline': 300,
+}

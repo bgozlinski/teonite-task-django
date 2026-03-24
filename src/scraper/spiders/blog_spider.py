4a704885c603d522f872d822dccff837457d3854
bgozlinski <bartlomiej.gozlinski@gmail.com>
2026-03-24 13:17:27 +0100

    feat: implement BlogSpider for Netflix Tech Blog

---
+++ src/scraper/spiders/blog_spider.py
@@ -0,0 +1,26 @@
+import scrapy
+from scrapy import Selector
+from scrapy.http import Request, Response
+from scraper.items import ArticleItem
+
+class BlogSpider(scrapy.Spider):
+    name = "blog"
+    start_urls = ["https://netflixtechblog.com/feed"]
+    custom_settings = {"ITEM_PIPELINES": {}}
+
+    def parse(self, response: Response):
+        sel = Selector(response, type='xml')
+        sel.register_namespace('content', 'http://purl.org/rss/1.0/modules/content/')
+        for item in sel.css('item'):
+            link = item.css('link::text').get()
+            if link:
+                yield response.follow(link, callback=self.parse_article)
+
+    def parse_article(self, response: Response):
+        yield ArticleItem(
+            title=(response.css('title::text').get() or '').split('|')[0].strip(),
+            url=response.url,
+            author_name=response.css('meta[property="og:description"]::attr(content)').get(),
+            content=' '.join(response.css('article p::text').getall()),
+        )
+
